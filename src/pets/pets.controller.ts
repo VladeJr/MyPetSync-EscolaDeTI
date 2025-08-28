e09f51f7382fda74be0643d14962af3521fb12c4
Vlade <nekaytsu@gmail.com>
2025-08-28 13:36:26 -0300

    feat: Criação do Crud de Pets

--- src/pets/pets.controller.ts
+++ src/pets/pets.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
+import { PetsService } from './pets.service';
+import type { Pet } from './pet.model';
 
 @Controller('pets')
-export class PetsController {}
+export class PetsController {
+  constructor(private readonly petsService: PetsService) {}
+
+  @Post()
+  create(@Body() petData: Partial<Pet>) {
+    return this.petsService.create(petData);
+  }
+
+  @Get()
+  findAll(): Pet[] {
+    return this.petsService.findAll();
+  }
+
+  @Get(':index')
+  findOne(@Param('index') index: string): Pet {
+    return this.petsService.findOne(Number(index));
+  }
+
+  @Put(':index')
+  update(@Param('index') index: string, @Body() petData: Partial<Pet>): Pet {
+    return this.petsService.update(Number(index), petData);
+  }
+
+  @Delete(':index')
+  remove(@Param('index') index: string) {
+    this.petsService.remove(Number(index));
+    return { message: `Pet ${index} removido` };
+  }
+}
--- src/pets/pets.service.ts
+++ src/pets/pets.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { Pet } from './pet.model';
 
 @Injectable()
-export class PetsService {}
+export class PetsService {
+  private pets: Pet[] = []; 
+
+  create(petData: Partial<Pet>): Pet {
+    const newPet: Pet = { ...petData } as Pet;
+    this.pets.push(newPet);
+    return newPet;
+  }
+
+  findAll(): Pet[] {
+    return this.pets;
+  }
+
+  findOne(index: number): Pet {
+    return this.pets[index];
+  }
+
+  update(index: number, petData: Partial<Pet>): Pet {
+    this.pets[index] = { ...this.pets[index], ...petData } as Pet;
+    return this.pets[index];
+  }
+
+  remove(index: number): void {
+    this.pets.splice(index, 1);
+  }
+}
