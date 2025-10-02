0c013eef69a0d771930193f4e5306b37151b65fa
João Renzetti <jaorenzetti@outlook.com>
2025-10-02 18:36:45 -0300

    chore: alteração no auth guard

--- src/auth/guards/auth.guard.ts
+++ src/auth/guards/auth.guard.ts
@@ -2,5 +2,4 @@ import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-// o nome 'jwt' tem que bater com o que foi definido na JwtStrategy
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt.secret') {}
