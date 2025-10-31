e20a65cdaf65949404a701d617c3155af5bdcd19
GustavoZanardi15 <ulianzanardig@gmail.com>
2025-10-31 15:09:38 -0300

    refactor(providers): ajustar dto de update para aceitar campos opcionais e limpar payload

--- src/providers/dto/update-provider.dto.ts
+++ src/providers/dto/update-provider.dto.ts
@@ -1,4 +1,35 @@
-import { PartialType } from '@nestjs/swagger';
+import { PartialType, ApiPropertyOptional } from '@nestjs/swagger';
 import { CreateProviderDto } from './create-provider.dto';
+import { IsString, IsOptional, MaxLength } from 'class-validator';
 
-export class UpdateProviderDto extends PartialType(CreateProviderDto) {}
+export class UpdateProviderDto extends PartialType(CreateProviderDto) {
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  city?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  state?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  whatsapp?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  email?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  bio?: string;
+}
