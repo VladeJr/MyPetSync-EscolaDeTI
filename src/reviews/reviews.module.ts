3901c1f8022daf65cd31372571ed41cbe1c81f5a
GustavoZanardi15 <ulianzanardig@gmail.com>
2025-11-05 16:40:19 -0300

    chore(reviews): importar providers module para injecao de servico

--- src/reviews/reviews.module.ts
+++ src/reviews/reviews.module.ts
@@ -3,9 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ReviewsController } from './reviews.controller';
 import { ReviewsService } from './reviews.service';
 import { Review, ReviewSchema } from './schemas/review.schema';
+import { ProvidersModule } from '../providers/providers.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Review.name, schema: ReviewSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: Review.name, schema: ReviewSchema }]),
+    ProvidersModule,
+  ],
   controllers: [ReviewsController],
   providers: [ReviewsService],
   exports: [ReviewsService],
