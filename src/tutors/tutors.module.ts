11f213cf5dd887d191b0115045b0fddb1aed0ea2
AlissonKobayashi <alisson.kobayashi@gmail.com>
2025-11-17 09:34:29 -0300

    fix(users): corrigir bug de editar somente users no banco de dados

--- src/tutors/tutors.module.ts
+++ src/tutors/tutors.module.ts
@@ -3,12 +3,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Tutor, TutorSchema } from './schemas/tutor.schema';
 import { TutorsService } from './tutors.service';
 import { TutorsController } from './tutors.controller';
-import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Tutor.name, schema: TutorSchema }]),
-    UsersModule,
   ],
   controllers: [TutorsController],
   providers: [TutorsService],
