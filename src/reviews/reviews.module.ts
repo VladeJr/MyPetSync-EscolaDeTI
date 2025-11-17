2bb0946f655be80eeecbc91a3f553bf1b8faefb6
AlissonKobayashi <alisson.kobayashi@gmail.com>
2025-11-17 17:08:44 -0300

    fix(frontend): avaliar mais de um prestador

--- src/reviews/reviews.module.ts
+++ src/reviews/reviews.module.ts
@@ -1,21 +1,21 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common'; // ✅ Adicione forwardRef aqui
 import { MongooseModule } from '@nestjs/mongoose';
 import { ReviewsController } from './reviews.controller';
 import { ReviewsService } from './reviews.service';
 import { Review, ReviewSchema } from './schemas/review.schema';
-import { UsersModule } from '../users/users.module'; 
+import { UsersModule } from '../users/users.module';
 import { AppointmentsModule } from '../appointments/appointments.module';
 import { ProvidersModule } from 'src/providers/providers.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Review.name, schema: ReviewSchema }]),
-    ProvidersModule,
-    UsersModule, 
-    AppointmentsModule, 
+    forwardRef(() => ProvidersModule), // ✅ CORRIGIDO: Era o índice [1] que causou o erro.
+    UsersModule,
+    forwardRef(() => AppointmentsModule), // ✅ CORRIGIDO: Necessário por causa da injeção no AppointmentsModule.
   ],
   controllers: [ReviewsController],
   providers: [ReviewsService],
   exports: [ReviewsService],
 })
-export class ReviewsModule {}
+export class ReviewsModule { }
